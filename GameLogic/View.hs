85fc8635dec6392ff0085104e6091816721021e3
Christopher Hebert <hebert.christopherj@gmail.com>
2013-10-16 16:54:03 -0700

    postiveFacing refactor

--- GameLogic/View.hs
+++ GameLogic/View.hs
@@ -27,29 +27,33 @@ getView :: GameState -> [[Color]]
 getView (GameState player grid) =
     mapInd (mapInd . beadColor) viewSection
   where
-    playerFacing = playerGetFacing player
+    positiveFacing = playerGetFacing player == Positive
 
     (maxX, _, maxZ) = gridDimensions grid
     invertZ z = maxZ - z - 1
 
     (playerX, playerY, playerZ') = playerGetPosition player
     playerZ
-        | playerFacing == Positive = playerZ'
+        | positiveFacing = playerZ'
         | otherwise =  invertZ playerZ'
 
     viewSection' = grid !! playerX
     viewSection
-        | playerFacing == Positive = viewSection'
+        | positiveFacing = viewSection'
         | otherwise = map reverse viewSection'
 
     distanceToWall :: GridY -> GridZ -> Int
-    distanceToWall y z =
-        let -- x slice with the given yz values
+    distanceToWall y z' =
+        let z = if positiveFacing
+                then z'
+                else invertZ z'
+
+            -- x slice with the given yz values
             xSlice :: [GridBead]
             xSlice = map (\x -> fromMaybe Wall $ gridGet grid x y z) [(-1)..maxX]
 
             delta
-                | playerFacing == Positive = (+ 1)
+                | positiveFacing = (+ 1)
                 | otherwise = (+ (-1))
 
             distance :: Int -> Int -> Int
@@ -62,5 +66,4 @@ getView (GameState player grid) =
     beadColor :: GridY -> GridZ -> GridBead -> Color
     beadColor y z bead
         | (y, z) == (playerY, playerZ) = PlayerColor
-        | playerFacing == Positive = WallColor $ distanceToWall y z
-        | playerFacing == Negative = WallColor $ distanceToWall y (invertZ z)
+        | otherwise = WallColor $ distanceToWall y z
