d63e678eaef950d50a6d50b470f70ce6cae5ad1d
Christopher Hebert <hebert.christopherj@gmail.com>
2013-10-16 00:06:54 -0700

    view stuff. Just GridBead. Colors

--- GameLogic/GameSpec.hs
+++ GameLogic/GameSpec.hs
@@ -5,9 +5,17 @@ import Main.Types
 import GameLogic.Grid ( replace
                       , gridGet
                       , gridSet
+                      , gridDimensions
                       , Grid(..)
                       )
 import GameLogic.Player
+
+import Debug.Trace
+
+data Color = WallFG
+           | EmptyColor
+           | PlayerColor
+  deriving (Show, Eq)
 
 data GridBead = Wall
               | Empty
@@ -16,18 +24,74 @@ data GridBead = Wall
 data GameState = GameState { _player :: Player
                            , _grid :: Grid GridBead
                            }
+  deriving (Show, Eq)
+
+mapInd :: (Int -> a -> b) -> [a] -> [b]
+mapInd f l = zipWith f [0..] l
 
 isValidPlayerPosition :: GameState -> Bool
 isValidPlayerPosition gameState =
-    gridGet grid x y z == Empty
+    gridGet grid x y z == Just Empty
   where grid = _grid gameState
         player = _player gameState
         (x, y, z) = (playerGetPosition player)
 
-getView :: Grid a -> Player -> [[a]]
-getView grid player = grid !! playerX
+leftButtonPressed :: GameState -> GameState
+leftButtonPressed gameState@(GameState player grid) =
+    if isValidPlayerPosition gameState'
+    then gameState'
+    else gameState
   where
-    (playerX, _, _) = playerGetPosition player
+    gameState' = GameState (playerMoveLeft player) grid
+
+rightButtonPressed :: GameState -> GameState
+rightButtonPressed gameState@(GameState player grid) =
+    if isValidPlayerPosition gameState'
+    then gameState'
+    else gameState
+  where
+    gameState' = GameState (playerMoveRight player) grid
+
+upButtonPressed :: GameState -> GameState
+upButtonPressed gameState@(GameState player grid) =
+    if isValidPlayerPosition gameState'
+    then gameState'
+    else gameState
+  where
+    gameState' = GameState (playerMoveUp player) grid
+
+downButtonPressed :: GameState -> GameState
+downButtonPressed gameState@(GameState player grid) =
+    if isValidPlayerPosition gameState'
+    then gameState'
+    else gameState
+  where
+    gameState' = GameState (playerMoveDown player) grid
+
+getView :: GameState -> [[Color]]
+getView (GameState player grid) =
+    mapInd (mapInd . beadColor) viewSection
+  where
+    playerFacing = playerGetFacing player
+
+    viewSection' = grid !! playerX
+    viewSection
+        | playerFacing == Positive = viewSection'
+        | otherwise = map reverse viewSection'
+
+    (playerX, playerY, playerZ) = playerGetPosition player
+    (_, _, maxZ) = gridDimensions grid
+
+    beadColor :: GridY -> GridZ -> GridBead -> Color
+    beadColor y z bead
+        | playerFacing == Positive &&
+          y == playerY &&
+          z == playerZ = PlayerColor
+        | playerFacing == Negative &&
+          y == playerY &&
+          z == (maxZ - playerZ - 1) = PlayerColor
+        | bead == Wall  = WallFG
+        | bead == Empty = EmptyColor
 
 spec :: Spec
 spec = do
@@ -54,20 +118,41 @@ spec = do
                          , [ Empty, Empty, Empty ]
                          ]
                        ]
-            testPlayer :: (GridX, GridY, GridZ) -> Player
-            testPlayer pos = Player pos Positive
+            gameState :: (GridX, GridY, GridZ) -> GameState
+            gameState pos = GameState (Player pos Positive) testGrid
         describe "player positions" $ do
             describe "valid positions" $ do
                 it "Empty slots" $ do
-                    let gameState = GameState (testPlayer (1, 1, 1)) testGrid
-                    isValidPlayerPosition gameState `shouldBe` True
+                    let gs = gameState (1, 1, 1)
+                    isValidPlayerPosition gs `shouldBe` True
             describe "invalid positions" $ do
                 it "Wall slots" $ do
-                    let gameState = GameState (testPlayer (1, 0, 1)) testGrid
-                    isValidPlayerPosition gameState `shouldBe` False
+                    let gs = gameState (1, 0, 1)
+                    isValidPlayerPosition gs `shouldBe` False
                 it "OoB X" $ do
-                    let gameState = GameState (testPlayer (3, 1, 1)) testGrid
-                    isValidPlayerPosition gameState `shouldBe` False
+                    let gs = gameState (3, 0, 1)
+                    isValidPlayerPosition gs `shouldBe` False
+                it "OoB Y" $ do
+                    let gs = gameState (1, (-1), 1)
+                    isValidPlayerPosition gs `shouldBe` False
+                it "OoB Z" $ do
+                    let gs = gameState (1, 1, 3)
+                    isValidPlayerPosition gs `shouldBe` False
+        describe "player movement" $ do
+            it "allows player movement to a valid position" $ do
+                leftButtonPressed (gameState (1, 1, 1)) `shouldBe` gameState (1, 1, 0)
+            it "disallows player movement to an invalid position" $ do
+                leftButtonPressed (gameState (1, 1, 0)) `shouldBe` gameState (1, 1, 0)
+
+            describe "directions" $ do
+                it "left" $ do
+                    leftButtonPressed (gameState (1, 1, 1)) `shouldBe` gameState (1, 1, 0)
+                it "right" $ do
+                    rightButtonPressed (gameState (1, 1, 1)) `shouldBe` gameState (1, 1, 2)
+                it "up" $ do
+                    upButtonPressed (gameState (1, 2, 1)) `shouldBe` gameState (1, 1, 1)
+                it "down" $ do
+                    downButtonPressed (gameState (1, 1, 1)) `shouldBe` gameState (1, 2, 1)
 
     describe "grid" $ do
         let testGrid :: Grid Int
@@ -80,8 +165,12 @@ spec = do
                          ]
                        ]
         it "can get the value at a given (x,y,z)" $ do
-            gridGet testGrid 0 1 1 `shouldBe` 011
-            gridGet testGrid 1 1 0 `shouldBe` 110
+            gridGet testGrid 0 1 1 `shouldBe` Just 011
+            gridGet testGrid 1 1 0 `shouldBe` Just 110
+
+        it "can get Nothing out of bounds" $ do
+            gridGet testGrid (-1) 1 1 `shouldBe` Nothing
+            gridGet testGrid   0  1 2 `shouldBe` Nothing
 
         it "can set the value at a given (x,y,z)" $ do
             gridSet testGrid 1 1 0 42 `shouldBe`
@@ -104,8 +193,8 @@ spec = do
                          , [ Empty, Wall ]
                          ]
                        ]
-            gridGet grid 0 1 1 `shouldBe` Wall
-            gridGet grid 1 0 0 `shouldBe` Empty
+            gridGet grid 0 1 1 `shouldBe` Just Wall
+            gridGet grid 1 0 0 `shouldBe` Just Empty
 
     describe "the player" $ do
         let testPlayer :: Player
@@ -137,24 +226,36 @@ spec = do
                 playerGetPosition (playerMoveRight player') `shouldBe` (1, 1, 0)
 
     describe "Game View" $ do
-        let testGrid :: Grid Int
-            testGrid = [ [ [ 000, 001, 002 ]
-                         , [ 010, 011, 012 ]
-                         , [ 020, 021, 022 ]
+        let testGrid :: Grid GridBead
+            testGrid = [ [ [ Wall, Wall, Wall ]
+                         , [ Wall, Empty, Wall ]
+                         , [ Wall, Wall, Wall ]
+                         ]
+                                       -- \/player
+                       , [ [ Empty, Empty, Empty ]
+                         , [ Empty, Wall, Empty ]
+                         , [ Empty, Empty, Empty ]
                          ]
 
-                       , [ [ 100, 101, 102 ]
-                         , [ 110, 111, 112 ]
-                         , [ 120, 121, 122 ]
-                         ]
-
-                       , [ [ 100, 101, 102 ]
-                         , [ 110, 111, 112 ]
-                         , [ 120, 121, 122 ]
+                       , [ [ Empty, Empty, Wall ]
+                         , [ Empty, Empty, Wall ]
+                         , [ Empty, Empty, Wall ]
                          ]
                        ]
-            testPlayer :: Player
-            testPlayer = Player (1, 1, 1) Positive
-
-        it "returns the yz plane from the grid and the player" $ do
-            getView testGrid testPlayer `shouldBe` testGrid !! 1
+        describe "positive facing" $ do
+            let gameState = GameState (Player (1, 0, 2) Positive) testGrid
+            it "draws empty walls with the empty color" $ do
+                ((getView gameState) !! 0) !! 0 `shouldBe` EmptyColor
+            it "draws walls with the wall foreground color" $ do
+                ((getView gameState) !! 1) !! 1 `shouldBe` WallFG
+            it "draws the player with player color with Positive facing" $ do
+                ((getView gameState) !! 0) !! 2 `shouldBe` PlayerColor
+        describe "negative facing" $ do
+            let gameState = GameState (Player (2, 0, 0) Negative) testGrid
+            it "draws the player with player color with Negative facing" $ do
+                ((getView gameState) !! 0) !! 2 `shouldBe` PlayerColor
+                ((getView gameState) !! 0) !! 0 `shouldBe` WallFG
+            it "draws walls with the wall foreground color" $ do
+                ((getView gameState) !! 0) !! 0 `shouldBe` WallFG
+            it "draws empties with the empty color" $ do
+                ((getView gameState) !! 1) !! 2 `shouldBe` EmptyColor
--- GameLogic/Grid.hs
+++ GameLogic/Grid.hs
@@ -4,21 +4,38 @@ import Main.Types
 
 type Grid a = [[[a]]]
 
+gridDimensions :: Grid a -> (GridX, GridY, GridZ)
+gridDimensions grid = (sizeX, sizeY, sizeZ) 
+  where
+    sizeX = length grid
+    sizeY = length $ grid !! 0
+    sizeZ = length $ (grid !! 0) !! 0
+
+validBounds :: Grid a -> GridX -> GridY -> GridZ -> Bool
+validBounds grid x y z =
+    x >= 0 && x < sizeX &&
+    y >= 0 && y < sizeY &&
+    z >= 0 && z < sizeZ
+  where
+    (sizeX, sizeY, sizeZ) = gridDimensions grid
+
 replace :: [a] -> Int -> a -> [a]
 replace list index val =
     (take index list) ++ val:(drop (index + 1) list)
 
-gridGet :: Grid a -> GridX -> GridY -> GridZ -> a
-gridGet map x y z = ((map !! x) !! y) !! z
+gridGet :: Grid a -> GridX -> GridY -> GridZ -> Maybe a
+gridGet grid x y z
+    | validBounds grid x y z = Just $ ((grid !! x) !! y) !! z
+    | otherwise = Nothing
 
 gridSet :: Grid a -> GridX -> GridY -> GridZ -> a -> Grid a
-gridSet map x y z value = newMap
+gridSet grid x y z value = newMap
   where
-    xList = map !! x
+    xList = grid !! x
     yList = xList !! y
 
     newZ = value
     newYList = replace yList z newZ
     newXList = replace xList y newYList
 
-    newMap = replace map x newXList
+    newMap = replace grid x newXList
