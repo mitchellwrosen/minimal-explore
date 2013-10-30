ffc36eeb8d3ee9f70c9b3b6ebee3e2e81bbc833c
Christopher Hebert <hebert.christopherj@gmail.com>
2013-10-29 17:48:36 -0700

    extracted MoveSpec from PlayerSpec

---
+++ test/GameLogic/MoveSpec.hs
@@ -0,0 +1,41 @@
+module GameLogic.MoveSpec ( spec ) where
+
+import Test.Hspec
+
+import GameLogic.Types ( Facing(..)
+                       , Position(..)
+                       )
+import GameLogic.Move ( moveForward
+                      , moveUp
+                      , moveDown
+                      , moveLeft
+                      , moveRight
+                      )
+
+spec :: Spec
+spec = describe "moves" $ do
+    describe "positive" $ do
+        let doMove move = move Positive (0, 0, 0)
+        it "moveUp" $
+            (doMove moveUp) `shouldBe` (0, -1, 0)
+        it "moveDown" $
+            (doMove moveDown) `shouldBe` (0, 1, 0)
+        it "moveRight" $
+            (doMove moveRight) `shouldBe` (0, 0, 1)
+        it "moveLeft" $
+            (doMove moveLeft) `shouldBe` (0, 0, -1)
+        it "moveForward" $
+            (doMove moveForward) `shouldBe` (1, 0, 0)
+
+    describe "negative" $ do
+        let doMove move = move Negative (0, 0, 0)
+        it "moveUp" $
+            (doMove moveUp) `shouldBe` (0, -1, 0)
+        it "moveDown" $
+            (doMove moveDown) `shouldBe` (0, 1, 0)
+        it "moveRight" $
+            (doMove moveRight) `shouldBe` (0, 0, -1)
+        it "moveLeft" $
+            (doMove moveLeft) `shouldBe` (0, 0, 1)
+        it "moveForward" $
+            (doMove moveForward) `shouldBe` (-1, 0, 0)
--- test/GameLogic/PlayerSpec.hs
+++ test/GameLogic/PlayerSpec.hs
@@ -3,12 +3,7 @@ module GameLogic.PlayerSpec ( spec ) where
 import Test.Hspec
 
 import GameLogic.Types ( Facing(..) )
-import GameLogic.Move ( moveForward
-                      , moveUp
-                      , moveDown
-                      , moveLeft
-                      , moveRight
-                      )
+import GameLogic.Move ( moveRight )
 
 import GameLogic.Player ( Player
                         , makePlayer
@@ -20,7 +15,6 @@ import GameLogic.Player ( Player
 
 import Control.Lens ( (^.) )
 
--- TODO(R): split out into a MoveSpec.hs
 spec :: Spec
 spec = 
     describe "the player" $ do
@@ -29,16 +23,8 @@ spec =
         it "has an xyz position" $ do
             testPlayer ^. playerPosition `shouldBe` (1, 1, 1)
 
-        it "moves up and down in the y direction one unit" $ do
-            (playerApplyMove testPlayer moveDown) ^. playerPosition `shouldBe` (1, 2, 1)
-            (playerApplyMove testPlayer moveUp) ^. playerPosition `shouldBe` (1, 0, 1)
-
-        it "moves left and right in the z direction one unit" $ do
-            (playerApplyMove testPlayer moveLeft) ^. playerPosition `shouldBe` (1, 1, 0)
+        it "moves right in the z direction one unit" $ do
             (playerApplyMove testPlayer moveRight) ^. playerPosition `shouldBe` (1, 1, 2)
-
-        it "can move forward in the x direction one unit" $ do
-            (playerApplyMove testPlayer moveForward) ^. playerPosition `shouldBe` (2, 1, 1)
 
         it "faces +x direction" $ do
             testPlayer ^. playerFacing `shouldBe` Positive
@@ -49,8 +35,4 @@ spec =
                 player' ^. playerFacing `shouldBe` Negative
 
             it "reverses left/right for the z direction" $ do
-                (playerApplyMove player' moveLeft) ^. playerPosition `shouldBe` (1, 1, 2)
                 (playerApplyMove player' moveRight) ^. playerPosition `shouldBe` (1, 1, 0)
-
-            it "moves forward in the -x direction" $ do
-                (playerApplyMove player' moveForward) ^. playerPosition `shouldBe` (0, 1, 1)
