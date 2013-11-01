35384f4e4a4efe370f01f311def91431d2249ed1
Christopher Hebert <hebert.christopherj@gmail.com>
2013-11-01 10:03:46 -0700

    more coverage over Types

--- test/GameLogic/TypesSpec.hs
+++ test/GameLogic/TypesSpec.hs
@@ -5,9 +5,14 @@ import Test.Hspec
 import GameLogic.Types ( Door(..)
                        , doorMapName
                        , doorId
+                       , posX
+                       , posY
+                       , posZ
                        )
 
-import Control.Lens ( (^.) )
+import Control.Lens ( (^.)
+                    , (.~)
+                    )
 
 spec :: Spec
 spec = do
@@ -15,6 +20,25 @@ spec = do
         let door = Door "map" "id" (255, 255, 255)
         it "has a map name" $ do
             door^.doorMapName `shouldBe` "map"
-
         it "has a unqiue id" $ do
             door^.doorId `shouldBe` "id"
+
+        describe "lenses" $ do
+            it "doorMapName" $ do
+                let name = "new name"
+                (doorMapName.~name $ door)^.doorMapName `shouldBe` name
+            it "doorId" $ do
+                let ident = "new ident"
+                (doorId.~ident $ door)^.doorId `shouldBe` ident
+    describe "door" $ do
+        describe "lenses" $ do
+            let pos = (0, 0, 0)
+            it "posX" $ do
+                (posX.~1 $ pos) `shouldBe` (1, 0, 0)
+                (posX.~1 $ pos)^.posX `shouldBe` 1
+            it "posY" $ do
+                (posY.~1 $ pos) `shouldBe` (0, 1, 0)
+                (posY.~1 $ pos)^.posY `shouldBe` 1
+            it "posZ" $ do
+                (posZ.~1 $ pos) `shouldBe` (0, 0, 1)
+                (posZ.~1 $ pos)^.posZ `shouldBe` 1
